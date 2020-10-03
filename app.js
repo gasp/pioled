54613fb207dd464f5e8bfb8fbabb80e6ff83c3fd
gaspard <gaspard@gmail.com>
2020-10-03 17:24:53 +0200

    feat: apply post instructions into oled

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-// const Oled = require('./lib/oled')
+const Oled = require('./lib/oled')
 const clock = require('./lib/clock')
 const server = require('./lib/server')
+const transcode = require('./lib/transcode')
 
-// const oled = new Oled()
+const oled = new Oled()
 const boot = clock(() => {
   // todo: play boot sequence on oled here
   console.log('booted')
 })
 
 boot.start()
-const cb = a => console.log(a)
+const cb = a => transcode(a, oled)
 server(cb)
